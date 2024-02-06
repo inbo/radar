0dad73a319f6dfc3a5aca588a9b9ac5dad90f157
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2024-02-06 11:35:03 +0100

    create add_vleemo_track_points()

---
+++ R/add_vleemo_track_points.R
@@ -0,0 +1,88 @@
+#' @inheritParams equal_time_track
+#' @inheritParams add_vleemoe_observed_track
+#' @importFrom assertthat assert_that
+#' @importFrom dplyr transmute
+#' @importFrom sf st_coordinates st_read
+#' @importFrom rlang .data
+#' @importFrom RSQLite dbAppendTable dbClearResult dbGetQuery dbSendQuery
+#' @export
+add_vleemo_track_points <- function(local, remote, rate = 2) {
+  assert_that(
+    inherits(local, "SQLiteConnection"),
+    inherits(remote, "PostgreSQLConnection")
+  )
+  "CREATE TABLE IF NOT EXISTS track_equal_time
+  (
+    id INTEGER PRIMARY KEY AUTOINCREMENT, track_id INTEGER NOT NULL,
+    scheme_id INTEGER NOT NULL, part INTEGER NOT NULL,
+    rate INTEGER NOT NULL CHECK (rate > 0)
+  )" |>
+    dbSendQuery(conn = local) |>
+    dbClearResult()
+  "CREATE UNIQUE INDEX IF NOT EXISTS track_equal_time_idx ON track_equal_time
+  (track_id, scheme_id, part, rate)" |>
+    dbSendQuery(conn = local) |>
+    dbClearResult()
+  "CREATE TABLE IF NOT EXISTS track_equal_time_point
+  (
+    equal_time_id INTEGER NOT NULL, t NUMERIC NOT NULL, x NUMERIC NOT NULL,
+    y NUMERIC NOT NULL, z NUMERIC NOT NULL, step_2d NUMERIC NOT NULL,
+    step_3d NUMERIC NOT NULL, yaw NUMERIC NOT NULL, pitch NUMERIC NOT NULL,
+    delta_yaw NUMERIC NOT NULL, delta_pitch NUMERIC NOT NULL
+  )" |>
+    dbSendQuery(conn = local) |>
+    dbClearResult()
+  "CREATE UNIQUE INDEX IF NOT EXISTS track_equal_time_point_idx
+  ON track_equal_time_point (equal_time_id, t)" |>
+    dbSendQuery(conn = local) |>
+    dbClearResult()
+  while (TRUE) {
+    dbGetQuery(
+      conn = local,
+      "SELECT t.id, s.scheme, t.scheme_id
+  FROM track_bbox AS t
+  INNER JOIN scheme AS s ON t.scheme_id = s.id
+  LEFT JOIN track_equal_time AS c ON
+    t.scheme_id = c.scheme_id AND t.id = c.track_id
+  WHERE c.rate IS NULL
+  LIMIT 1"
+    ) -> track_id
+    if (nrow(track_id) == 0) {
+      break
+    }
+    message(track_id$scheme, " ", track_id$id)
+    sprintf(
+      "SELECT
+    id, ST_Transform(trajectory, 31370) AS track, trajectory_time AS time,
+    trajectory_radarid AS radar
+  FROM %s.track
+  WHERE id = %i",
+      track_id$scheme, track_id$id
+    ) -> query
+    tracks <- st_read(dsn = remote, query = query)
+    strsplit(tracks$radar, ",") |>
+      unlist() |>
+      gsub(pattern = "(\\{|\\})", replacement = "") |>
+      as.integer() -> radar
+    assert_that(length(unique(radar)) == 1)
+    strsplit(tracks$time, ",") |>
+      unlist() |>
+      gsub(pattern = "(\\{|\\})", replacement = "") |>
+      as.numeric() -> time
+    st_coordinates(tracks) |>
+      as.data.frame() |>
+      transmute(x = .data$X, y = .data$Y, z = .data$Z, t = time) |>
+      equal_time_track(rate = rate) -> raw_track
+    track_id |>
+      transmute(track_id = .data$id, .data$scheme_id, rate = rate) |>
+      dbAppendTable(conn = local, name = "track_equal_time")
+    sprintf(
+      "SELECT id FROM track_equal_time WHERE track_id = %i AND scheme_id = %i",
+      track_id$id, track_id$scheme_id
+    ) |>
+      dbGetQuery(conn = local) -> equal_id
+    raw_track |>
+      mutate(equal_time_id = equal_id$id) |>
+      dbAppendTable(conn = local, name = "track_equal_time_point")
+  }
+}
--- R/equal_time_track.R
+++ R/equal_time_track.R
@@ -2,6 +2,9 @@
 #'
 #' The resulting track has
 #'
+#'  - the part of the track (`part`).
+#'    We split the part at the points where the ground speed is above 30 meter
+#'    per second.
 #'  - the space-time coordinates (`t`, `x`, `y` and `z`)
 #'  - the step length in 2D and 3D (`step_2d` and `step_3d`)
 #'  - the heading in the x-y plane (`yaw`)
@@ -15,12 +18,14 @@
 #' @param rate the time between two points in the output track.
 #' @importFrom assertthat assert_that has_name is.number
 #' @importFrom dplyr arrange bind_rows filter group_by lag lead mutate
-#' inner_join rename select slice_max slice_min transmute ungroup
+#' inner_join rename select slice_max slice_min summarise transmute ungroup
 #' @importFrom rlang .data
+#' @importFrom tidyr unnest
 #' @export
 equal_time_track <- function(raw_track, rate = 2) {
   min_delta_t <- 0.9
   max_speed <- 30
+  min_duration <- 60
   assert_that(
     inherits(raw_track, "data.frame"), has_name(raw_track, "x"),
     has_name(raw_track, "y"), has_name(raw_track, "z"),
@@ -29,45 +34,61 @@ equal_time_track <- function(raw_track, rate = 2) {
   stopifnot(all(diff(raw_track$t) > min_delta_t))
   groundspeed <- sqrt(diff(raw_track$x) ^ 2 + diff(raw_track$y) ^ 2) /
     diff(raw_track$t)
-  stopifnot(all(groundspeed <= max_speed))
+  raw_track |>
+    mutate(
+      part = c(NA, groundspeed) |>
+        pmax(c(groundspeed, NA), na.rm = TRUE),
+      part = cumsum(.data$part > max_speed)
+    ) -> raw_track
+  raw_track |>
+    group_by(.data$part) |>
+    summarise(start = min(.data$t), end = max(.data$t)) |>
+    mutate(
+      start = ceiling(.data$start / rate) * rate,
+      end = floor(.data$end / rate) * rate
+    ) |>
+    filter(.data$end - .data$start >= min_duration) -> parts
   raw_track |>
     mutate(tr = ceiling(.data$t / rate) * rate) |>
-    group_by(.data$tr) |>
+    group_by(.data$part, .data$tr) |>
     slice_max(.data$t, n = 1) |>
     ungroup() |>
     rename(t0 = "t", x0 = "x", y0 = "y", z0 = "z") |>
     inner_join(
-      x = data.frame(
-        tr = seq(min(raw_track$t), max(raw_track$t), by = rate)
-      ),
-      by = "tr"
+      x = parts |>
+        transmute(
+          .data$part, tr = map2(.data$start, .data$end, ~seq(.x, .y, by = rate))
+        ) |>
+        unnest("tr"),
+      by = c("part", "tr")
     ) |>
     inner_join(
       raw_track |>
         mutate(tr = floor(.data$t / rate) * rate) |>
-        group_by(.data$tr) |>
+        group_by(.data$part, .data$tr) |>
         slice_min(.data$t, n = 1) |>
         ungroup() |>
         rename(t1 = "t", x1 = "x", y1 = "y", z1 = "z"),
-      by = "tr"
+      by = c("part", "tr")
     ) -> spaced
   spaced |>
     filter(.data$t0 < .data$t1) |>
     transmute(
+      .data$part, t = .data$tr,
       x = .data$x0 + (.data$x1 - .data$x0) * (.data$tr - .data$t0) /
         (.data$t1 - .data$t0),
       y = .data$y0 + (.data$y1 - .data$y0) * (.data$tr - .data$t0) /
         (.data$t1 - .data$t0),
       z = .data$z0 + (.data$z1 - .data$z0) * (.data$tr - .data$t0) /
-        (.data$t1 - .data$t0),
-      t = .data$tr
+        (.data$t1 - .data$t0)
     ) |>
     bind_rows(
       spaced |>
         filter(.data$t0 == .data$t1) |>
-        select(t = "tr", x = "x0", y = "y0", z = "z0")
+        select("part", t = "tr", x = "x0", y = "y0", z = "z0")
     ) |>
     arrange(.data$t) |>
+    group_by(.data$part) |>
     mutate(
       dx = .data$x - lead(.data$x), dy = .data$y - lead(.data$y),
       dz = .data$z - lead(.data$z),
@@ -79,8 +100,9 @@ equal_time_track <- function(raw_track, rate = 2) {
       delta_pitch = .data$pitch - lag(.data$pitch)
     ) |>
     filter(!is.na(.data$delta_yaw)) |>
+    ungroup() |>
     select(
-      "t", "x", "y", "z", "step_2d", "step_3d", "yaw", "pitch", "delta_yaw",
-      "delta_pitch"
+      "part", "t", "x", "y", "z", "step_2d", "step_3d", "yaw", "pitch",
+      "delta_yaw", "delta_pitch"
     )
 }
