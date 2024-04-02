c7c4a359218da1813b2fa1327b01351547d8b724
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2024-04-02 17:41:11 +0200

    update get_tracks_around_centre()

--- R/get_tracks_around_centre.R
+++ R/get_tracks_around_centre.R
@@ -10,8 +10,12 @@
 #' in the database.
 #' Select only tracks from species with a value equal or larger than this
 #' number.
+#' @param rate The time difference between points to calculate the step length
+#' and turning angles.
 #' @export
 #' @importFrom assertthat assert_that has_name is.count noNA
+#' @importFrom dplyr mutate
+#' @importFrom rlang .data
 #' @importFrom RSQLite dbGetQuery
 get_tracks_around_centre <- function(
   local, centre, max_distance = 200, max_height = 200, max_tracks = 100,
@@ -30,9 +34,8 @@ get_tracks_around_centre <- function(
   INNER JOIN track_equal_time AS te ON te.id = p.equal_time_id
   INNER JOIN track_time AS tt ON tt.id = te.track_id
   INNER JOIN species AS s ON tt.species_id = s.id
-  WHERE
-    s.relevant >= %i AND te.rate = %i AND p.z <= %i AND
-    SQRT(POWER(p.x - %f, 2) + POWER(p.y - %f, 2)) <= %f
+  WHERE s.relevant >= %i AND p.z <= %i  AND
+      SQRT(POWER(p.x - %f, 2) + POWER(p.y - %f, 2)) <= %f
   GROUP BY p.equal_time_id
   LIMIT %i
 ),
@@ -45,14 +48,27 @@ cte_windmill_all AS (
   HAVING MIN(SQRT(POWER(p.x - w.x, 2) + POWER(p.y - w.y, 2)))
 )
 SELECT
-  p.equal_time_id, p.t, p.x, p.y, p.z, p.step_2d, p.yaw, p.delta_yaw,
-  w.x AS windmill_x, w.y AS windmill_y
-FROM cte_windmill_all AS c
-INNER JOIN track_equal_time_point AS p ON c.point_id = p.id
-INNER JOIN windmill AS w ON c.windmill_id = w.id
-" |>
-  sprintf(
-    relevant, rate, max_height, centre$x, centre$y, max_distance, max_tracks
-  ) |>
-  dbGetQuery(conn = local)
+  p1.equal_time_id, p1.t, p1.x, p1.y, p1.z,
+  w.x AS windmill_x, w.y AS windmill_y,
+  SQRT(POWER(p2.x - p1.x, 2) + POWER(p2.y - p1.y, 2)) AS step_2d,
+  ATAN2(p1.y - p0.y, p1.x - p0.x) AS yaw,
+  ATAN2(p2.y - p1.y, p2.x - p1.x) - ATAN2(p1.y - p0.y, p1.x - p0.x) AS delta_yaw
+FROM cte_nearest AS c
+INNER JOIN track_equal_time_point AS p0 ON c.equal_time_id = p0.equal_time_id
+INNER JOIN track_equal_time_point AS p1
+  ON p0.equal_time_id = p1.equal_time_id AND p0.t + %i = p1.t
+INNER JOIN track_equal_time_point AS p2
+  ON p1.equal_time_id = p2.equal_time_id AND p1.t + %i = p2.t
+INNER JOIN cte_windmill_all AS cw ON p1.id = cw.point_id
+INNER JOIN windmill AS w ON cw.windmill_id = w.id" |>
+    sprintf(
+      relevant, max_height, centre$x, centre$y, max_distance, max_tracks, rate,
+      rate
+    ) |>
+    dbGetQuery(conn = local) |>
+    mutate(
+      delta_yaw = .data$delta_yaw + pi * ifelse(
+        .data$delta_yaw > pi, -2, ifelse(.data$delta_yaw < -pi, 2, 0)
+      )
+    )
 }
